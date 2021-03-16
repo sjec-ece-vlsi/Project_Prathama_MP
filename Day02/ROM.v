12e2c8c4118109e984e1afdd00ebe94370868ed3
vijayganeshpc <80514146+vijayganeshpc@users.noreply.github.com>
2021-03-16 09:13:37 +0530

    Update ROM.v

--- Day02/ROM.v
+++ Day02/ROM.v
@@ -1,7 +1,25 @@
+/*
+ ROM has 8K memory loaction with 16 bit width. 
+ Use parameter to define Location, width and address bits required for the ROM.
+
+*/
+
 module ROM_8k(
   
 );
   
+// parameter for Address width, Data_bus and Memeory Depth or locations
+  
+// Declare inputs and outputs including control signals
+  
+  
+// Declare Required internal wires or registers
+  
+  
+ // Logic goes here
+  
+  
+  //end of the logic
   
   
 endmodule
