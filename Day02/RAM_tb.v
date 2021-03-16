17ff82e756d8cd2d0477e53df535dfb924a05c53
vijayganeshpc <80514146+vijayganeshpc@users.noreply.github.com>
2021-03-16 09:26:44 +0530

    Create RAM_tb.v
    Testbench for 16K memory with 16-bit width

---
+++ Day02/RAM_tb.v
@@ -0,0 +1,24 @@
+/*
+ Create  a testbench to test 16k RAM with Control signals
+ 
+*/
+module RAM_tb();
+  
+  // Declare required registers, wires and parameters for Device under test (DUT) 
+  
+  // Call for DUT
+  
+  
+  // Initial blocks
+  
+  
+  //
+  
+  // Verfication code 
+  
+  
+  //
+  
+  //end of logic
+  
+endmodule
