e236eda05b22748dc3c7d30c68c142189f942bf8
vijayganeshpc <80514146+vijayganeshpc@users.noreply.github.com>
2021-03-16 09:23:29 +0530

    Create ROM_tb.v

---
+++ Day02/ROM_tb.v
@@ -0,0 +1,25 @@
+/*
+ Create  a testbench to test 8k ROM with Control signals
+ 
+
+*/
+module rom_tb();
+  
+  // Declare required registers, wires and parameters for Device under test (DUT) 
+  
+  // Call for DUT
+  
+  
+  // Initial blocks
+  
+  
+  //
+  
+  // Verfication code 
+  
+  
+  //
+  
+  //end of logic
+  
+endmodule
