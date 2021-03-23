ffecf9d96257dbd5547eb4653c9a867af0823e36
maheshaydk <maheshaydk@gmail.com>
2021-03-23 09:43:46 +0530

    Update add_sub.v

--- Source/add_sub.v
+++ Source/add_sub.v
@@ -1,41 +1,45 @@
-module add_sub(
-  input clk,mode,cs,
-  input [15:0]a,b,
-  output reg [15:0]sum,
-  output reg ready);
-  integer state=0;
-  always@(posedge clk)
+module add_sub(clk,SUM,COUT,A,B,CIN,SUB,cs,rdy);   
+ input [3:0]A,B;
+ input SUB,cs,CIN,clk;
+ output reg [3:0]SUM;
+ output reg COUT,rdy;
+ integer state;
+initial
+state=0;
+always@(posedge clk)
     begin
-      case(state)
-        0: begin
-          sum=16'bZ;
-          ready=1;
-          if(cs)
-            state=1;
-          else
-            state=0;
-        end
-        1: begin
-          ready=0;
-          
-          if(mode)
-            state=2;
-          else
-            state=3;
-        end
-        2:begin
-          sum= a+b;
-          state=4;
-        end
-        3:begin
-          sum=a+~b+1;
-          state=4;
-        end
-        4:begin
-          ready=1;
-          state=0;
-        end
-      endcase
+        case(state)
+            0:begin
+                if(cs)  state=1;
+                else    state=0;
+            end
+            1:begin
+                if(SUB) state=3;
+                else state=2;
+            end
+            2:state=0;
+            3:state=0;
+        endcase        
     end
+ always@(state)
+        begin 
+        case(state)  
+            0:begin
+             rdy=1;
+             SUM=16'bZ; 
+             COUT=1'bZ;
+             end
+            1:begin
+             rdy=0;
+             end  
+            2:begin
+             {COUT,SUM}=A+B+CIN;
+             rdy=1;
+             end 
+            3:begin
+              {COUT,SUM}=A+~B+1;
+              rdy=1;           
+              end 
+         endcase              
+        end
 endmodule
-        
